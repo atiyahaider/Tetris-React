8e8d8a7125a4d4a5b0c6d7584709f9d5f547441a
Atiya Haider <atiya_haider@hotmail.com>
2020-06-28 12:22:41 -0400

    Updates

---
+++ precache-manifest.d487b5efe8bb929989c24224c9c073d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dc5aaffdde6c55d0d256e9ef376da6",
+    "url": "/Tetris-React/index.html"
+  },
+  {
+    "revision": "aa9b4b2c440731a86722",
+    "url": "/Tetris-React/static/css/main.47838201.chunk.css"
+  },
+  {
+    "revision": "e1d61874b59c2b4119dd",
+    "url": "/Tetris-React/static/js/2.16936247.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tetris-React/static/js/2.16936247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9b4b2c440731a86722",
+    "url": "/Tetris-React/static/js/main.ac220c57.chunk.js"
+  },
+  {
+    "revision": "d9bbf0fe97dab88751f2",
+    "url": "/Tetris-React/static/js/runtime-main.5778fdec.js"
+  },
+  {
+    "revision": "7937a9fa58bb6a7e3477fbda222fca56",
+    "url": "/Tetris-React/static/media/trail-hGVk7r40q4k-unsplash.7937a9fa.jpg"
+  }
+]);
